b06952442160516f3906a335eaa7ebebadcba2b3
josefrvaldes <melodiasperdidas>
2015-04-07 09:38:46 +0100

    - Commit from Android Studio

--- app/src/main/java/com/example/z510/medialabtest1/MainActivity.java
+++ app/src/main/java/com/example/z510/medialabtest1/MainActivity.java
@@ -4,14 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Person p;
+    private Button btnCreatePerson;
+    private Button btnGetName;
+    private Button btnGetAge;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCreatePerson = (Button)findViewById(R.id.btn_create_person);
+        btnCreatePerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p = new Person();
+                // Hello
+            }
+        });
+
+        btnGetAge = (Button)findViewById(R.id.btn_get_age);
+        btnGetAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),String.valueOf(p.getAge()),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGetName = (Button)findViewById(R.id.btn_get_name);
+        btnGetName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),String.valueOf(p.getName()),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -19,6 +53,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
