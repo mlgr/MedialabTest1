33bc50df9427c0efc1e6f3d3fbc6cb8cc554453c
M.Luz <pasame@gmail.com>
2015-04-07 09:06:28 +0100

    V00003
    Changes on Person class

--- app/src/main/java/com/example/z510/medialabtest1/Person.java
+++ app/src/main/java/com/example/z510/medialabtest1/Person.java
@@ -5,6 +5,12 @@ package com.example.z510.medialabtest1;
  */
 public class Person {
     private String name;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
     private int age;
 
     public Person() {
@@ -20,6 +26,9 @@ public class Person {
         return age;
     }
 
-    
+    public void setName(String n) { name = n; }
+
+    public void setAge(int a) { age = a; }
+
 
 }
