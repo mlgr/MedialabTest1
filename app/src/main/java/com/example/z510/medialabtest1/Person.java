93f5c5e8093a9db71f69efd2e6f8ec3a7929b33b
M.Luz <pasame@gmail.com>
2015-04-07 08:58:43 +0100

    V00002
    Second test

---
+++ app/src/main/java/com/example/z510/medialabtest1/Person.java
@@ -0,0 +1,25 @@
+package com.example.z510.medialabtest1;
+
+/**
+ * Created by Z510 on 07/04/2015.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        name = "No name";
+        age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    
+
+}
